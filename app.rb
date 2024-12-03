0eea7709825a9bf7454214238cbe3247f5d46be8
HuaZhiSitu <hsitu@chicagobooth.edu>
2024-12-03 11:31:10 +0000

    finished

--- app.rb
+++ app.rb
@@ -4,10 +4,7 @@ require "sinatra/reloader"
 
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
 end
 
 get("/square/new") do
@@ -20,11 +17,27 @@ end
 get("/square_root/new") do
   erb(:squareroot)
 end
+get("/square_root/results") do
+  erb(:sqroot_results)
+end
 
 get("/payment/new") do
-    erb(:random)
+  erb(:payment)
+end
+get("/payment/results") do
+    r = params.fetch("user_apr").to_f / 100 / 12
+    n = params.fetch("user_years").to_i * 12
+    pv = params.fetch("user_pv").to_f
+    numerator = r * pv
+    denominator = 1 - (1 + r) ** -n
+    @payment = (numerator / denominator).round(2)
+    @interest_rate=(r*12*100).round(4)
+   erb(:payment_results) 
 end
 
 get("/random/new") do
   erb(:random)
+end
+get("/random/results") do
+  erb(:random_results)
 end
