736e5e06d3cc133eb82b1674c2dfaadf468b9030
HuaZhiSitu <hsitu@chicagobooth.edu>
2024-12-03 10:18:09 +0000

    Updated params fetch

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:squareroot)
+end
+
+get("/payment/new") do
+    erb(:random)
+end
+
+get("/random/new") do
+  erb(:random)
 end
